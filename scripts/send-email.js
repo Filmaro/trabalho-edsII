ed693fe065cae6b78429a2456aeb1bdffaf2649d
JoaoWudarski <joaowudarski@gmail.com>
2023-05-21 18:11:55 -0300

    Ajuste teste

--- scripts/send-email.js
+++ scripts/send-email.js
@@ -1,20 +1,25 @@
 const nodemailer = require('nodemailer');
 
+const smtpUsername = process.env.SMTP_USERNAME;
+const smtpPassword = process.env.SMTP_PASSWORD;
+const prNumber     = process.env.NUMBER_PR;
+const tag_name     = process.env.TAG_NAME;
+
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
   secure: false,
   auth: {
-    user: secrets.EMAIL_USERNAME,
-    pass: secrets.EMAIL_PASSWORD,
+    user: smtpUsername,
+    pass: smtpPassword,
   },
 });
 
 const mailOptions = {
   from: 'joaowudarski2@gmail.com',
   to: 'joaowudarski@gmail.com',
-  subject: 'Aprovação da release ' + steps.release.outputs.tag_name + ' para PRODUÇÃO',
-  text: 'Para que a nova release vá para o ambiente produtivo, é necessário uma aprovação manual no PR e deve ser dado o merge.\n\n Link do PR: https://github.com/Filmaro/trabalho-edsII/pull/' + steps.release.outputs.number,
+  subject: 'Aprovação da release ' + tag_name + ' para PRODUÇÃO',
+  text: 'Para que a nova release vá para o ambiente produtivo, é necessário uma aprovação manual no PR e deve ser dado o merge.\n\n Link do PR: https://github.com/Filmaro/trabalho-edsII/pull/' + prNumber,
 };
 
 transporter.sendMail(mailOptions, (error, info) => {
